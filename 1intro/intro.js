cd85aca50ab4c2b2678a74bac393bb105cf9af09
adeelfeb <at4563323@gmail.com>
2025-01-04 22:47:40 +0500

    Add intro files for tscript

---
+++ 1intro/intro.js
@@ -0,0 +1,3 @@
+console.log("hi there ");
+var user = { name: "temp", age: 45 };
+console.log("the name:", user.name, "age is:", user.age);
---
+++ 1intro/intro.tsx
@@ -0,0 +1,5 @@
+console.log("hi there ")
+
+const user = {name:"temp", age:45}
+
+console.log("the name:", user.name, "age is:", user.age)
